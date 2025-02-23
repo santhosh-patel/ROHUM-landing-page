f24c2bec3df0022133dfa2af01ab9cf471c78f09
sheikhfaris86 <sheikhfaris86@gmail.com>
2025-02-23 13:33:50 +0530

    made features section responsive

--- src/Pages/Features.jsx
+++ src/Pages/Features.jsx
@@ -89,17 +89,17 @@ const Features = () => {
 
   return (
     <div className="min-h-screen scroll-div mt-20 p-5">
-      <h3 className="text-center dark:text-gray-500 text-xl font-bold !font-mono">
+      <h3 className="text-center dark:text-gray-500 lg:text-xl font-bold !font-mono">
         AI FEATURES
       </h3>
-      <h1 className="text-center text-7xl mb-20">Features</h1>
+      <h1 className="text-center text-5xl lg:text-7xl mb-20">Features</h1>
 
       <div
         ref={containerRef}
-        className="justify-between h-[300vh] cards-div w-full flex items-start relative"
+        className="md:justify-center md:gap-10 lg:gap-0 lg:justify-between h-[250vh] lg:h-[300vh] cards-div w-full flex items-start relative"
       >
         <div
-          className={`left w-[40%] relative flex justify-center items-center mt-10 rounded-lg h-[60vh]`}
+          className={`left w-[40%] relative hidden lg:flex justify-center items-center mt-10 rounded-lg md:h-[30vh] lg:h-[60vh]`}
         >
           <div className="absolute top-0 z-[-2] h-full bg-white/20 w-full bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
           <div className="left-layer-div w-full h-full border border-[hsl(var(--border))] rounded-lg flex justify-center items-center">
@@ -156,104 +156,139 @@ const Features = () => {
         {/* right cards  */}
         <div
           ref={cardsRef}
-          className="right w-[50%] p-3 flex h-full justify-between flex-col"
+          className="right w-full md:w-4/5 lg:w-[50%] p-3 flex h-full justify-between flex-col"
         >
-          <div className="card bg-transparent h-[30%] flex justify-start items-center rounded-lg shadow-lg transform transition-all">
-            <div className="max-w-lg">
-              <h3 className="text-4xl font-bold mb-7">Realistic AI Avatars</h3>
-              <p className="text-gray-600 text-sm mb-3 dark:text-gray-300">
+          {/* card1 */}
+          <div className="card bg-transparent h-[30%] flex gap-5 w-full justify-start items-center rounded-lg shadow-lg transform transition-all">
+            <div className="max-w-lg pl-5 md:pl-0 ">
+              <h3 className="text-3xl text-start lg:text-4xl font-bold mb-7">
+                Realistic AI Avatars
+              </h3>
+              <p className="text-gray-600 text-start text-xs md:text-sm mb-3 dark:text-gray-400">
                 Our avatars understand and respond with emotional intelligence,
                 making interactions more human-like and engaging.
               </p>
               <ul>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Seamless text, voice, and video communication.
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Understands user emotions for personalized interactions.
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Adapts to user behavior for enhanced engagement.
                 </li>
               </ul>
             </div>
+            <div className=" hidden md:flex lg:hidden ">
+              <div className="border-glow-animation inline-flex p-[2px] rounded-xl">
+                <div className="text-7xl left-layer-div-card text-gray-50 p-5 rounded-xl">
+                  <RiRobot2Line />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card bg-transparent h-[30%] flex justify-start items-center rounded-lg shadow-lg transform transition-all">
-            <div className="max-w-lg">
-              <h3 className="text-4xl font-bold mb-7">Crypto Simplified</h3>
-              <p className="text-gray-600 text-sm mb-3 dark:text-gray-300">
+          {/* card12*/}
+          <div className="card bg-transparent h-[30%] flex gap-5 w-full justify-start items-center rounded-lg shadow-lg transform transition-all">
+            <div className="max-w-lg pl-5 md:pl-0 ">
+              <h3 className="text-3xl text-start lg:text-4xl font-bold mb-7">
+                Crypto Simplified
+              </h3>
+              <p className="text-gray-600 text-start text-xs md:text-sm mb-3 dark:text-gray-400">
                 Learn and invest in cryptocurrency easily with AI-powered
-                guidance.
+                guidance
               </p>
               <ul>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
-                  Step-by-step crypto education for beginners.
+                  Step-by-step crypto education for beginners.{" "}
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
-                  Real-time insights and investment strategies.
+                  Real-time insights and investment strategies.{" "}
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
-                  Safe and user-friendly investment recommendations.
+                  Safe and user-friendly investment recommendations.{" "}
                 </li>
               </ul>
             </div>
+            <div className=" hidden md:flex lg:hidden ">
+              <div className="border-glow-animation inline-flex p-[2px] rounded-xl">
+                <div className="text-7xl left-layer-div-card text-gray-50 p-5 rounded-xl">
+                  <FaEthereum />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card bg-transparent h-[30%] flex justify-start items-center rounded-lg shadow-lg transform transition-all">
-            <div className="max-w-lg">
-              <h3 className="text-4xl font-bold mb-7">
+          {/* card3 */}
+          <div className="card bg-transparent h-[30%] flex gap-5 w-full justify-start items-center rounded-lg shadow-lg transform transition-all">
+            <div className="max-w-lg pl-5 md:pl-0 ">
+              <h3 className="text-3xl text-start lg:text-4xl font-bold mb-7">
                 AI Business Assistance
               </h3>
-              <p className="text-gray-600 text-sm mb-3 dark:text-gray-300">
-                AI Business Assistance revolutionizes enterprise operations by
-                streamlining workflows and enhancing decision-making through
-                intelligent automation and data analysis.
+              <p className="text-gray-600 text-start text-xs md:text-sm mb-3 dark:text-gray-400">
+                AI Business Assistance leverages AI avatars to streamline
+                operations, enhance customer interactions, and automate routine
+                tasks efficiently.
               </p>
               <ul>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Automates business workflows and enhances productivity.
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
-                  AI-driven customer support and task management.
+                  AI-driven customer support and task management.{" "}
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
-                  Seamless integration with existing business platforms.
+                  Seamless integration with existing business platforms.{" "}
                 </li>
               </ul>
             </div>
+            <div className=" hidden md:flex lg:hidden ">
+              <div className="border-glow-animation inline-flex p-[2px] rounded-xl">
+                <div className="text-7xl left-layer-div-card text-gray-50 p-5 rounded-xl">
+                  <GiMoneyStack />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="card bg-transparent h-[30%] flex justify-start items-center rounded-lg shadow-lg transform transition-all">
-            <div className="max-w-lg">
-              <h3 className="text-4xl font-bold mb-7">
-                Interactive Learning & Training
+          {/* card4 */}
+          <div className="card bg-transparent h-[30%] flex gap-5 w-full justify-start items-center rounded-lg shadow-lg transform transition-all">
+            <div className="max-w-lg pl-5 md:pl-0 ">
+              <h3 className="text-3xl text-start lg:text-4xl font-bold mb-7">
+                Interactive Learning & Training{" "}
               </h3>
-              <p className="text-gray-600 text-sm mb-3 dark:text-gray-300">
-                Interactive learning and training using AI avatars enhance
-                engagement by providing real-time feedback, personalized
-                lessons, and lifelike simulations.
+              <p className="text-gray-600 text-start text-xs md:text-sm mb-3 dark:text-gray-400">
+                Interactive Learning & Training uses AI avatars to provide
+                immersive, personalized, and engaging educational experiences.
               </p>
               <ul>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Teaches dance, exercise, and Kung Fu step-by-step.{" "}
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Personalized training based on user progress.{" "}
                 </li>
-                <li className="text-sm dark:text-gray-300 flex items-center">
+                <li className="text-xs md:text-sm dark:text-gray-400 flex items-center">
                   <TiTick className="text-[var(--purple-foreground)] text-2xl " />
                   Adapts to user skill level for effective learning.{" "}
                 </li>
               </ul>
+            </div>
+            <div className=" hidden md:flex lg:hidden ">
+              <div className="border-glow-animation inline-flex p-[2px] rounded-xl">
+                <div className="text-7xl left-layer-div-card text-gray-50 p-5 rounded-xl">
+                  <GiTeacher />
+                </div>
+              </div>
             </div>
           </div>
         </div>
